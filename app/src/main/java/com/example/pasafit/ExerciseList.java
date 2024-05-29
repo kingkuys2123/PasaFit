c4a67a306a98e315e59348eab4237895536f4e6f
Kyle Matthew M. Quitco <kylequitco3212@gmail.com>
2024-05-30 00:46:43 +0800

    Finalized implementation of Exercises Categories, List and View

--- app/src/main/java/com/example/pasafit/ExerciseList.java
+++ app/src/main/java/com/example/pasafit/ExerciseList.java
@@ -7,15 +7,19 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
 import android.widget.TextView;
 
 public class ExerciseList extends AppCompatActivity {
 
     private TextView textViewExerciseListHeader;
+    private TextView exerciseListTitle1, exerciseListTitle2, exerciseListTitle3, exerciseListTitle4, exerciseListTitle5;
 
     private ImageButton nav_btn_home, nav_btn_trackers, nav_btn_calculator, nav_btn_profile, nav_btn_exercises;
+    private ImageView exerciseListImage1, exerciseListImage2, exerciseListImage3, exerciseListImage4, exerciseListImage5;
 
-    private Button btnBackExerciseList;
+    private Button btnBackExerciseList, btnExerciseListNext1, btnExerciseListNext2, btnExerciseListNext3, btnExerciseListNext4, btnExerciseListNext5;
+
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -29,6 +33,61 @@ public class ExerciseList extends AppCompatActivity {
         String exercise_type = intent.getStringExtra("exercise_type_key");
 
         textViewExerciseListHeader.setText(exercise_type + " Exercises");
+
+        if ("Balance".equals(exercise_type)) {
+            exerciseListTitle1.setText("Bird Dog");
+            exerciseListTitle2.setText("Lying Leg Raise");
+            exerciseListTitle3.setText("Single Leg Balance");
+            exerciseListTitle4.setText("Toe Taps");
+            exerciseListTitle5.setText("Tree Pose");
+
+            exerciseListImage1.setImageResource(R.drawable.balance_bird_dog);
+            exerciseListImage2.setImageResource(R.drawable.balance_lying_leg_raise);
+            exerciseListImage3.setImageResource(R.drawable.balance_single_leg_balance);
+            exerciseListImage4.setImageResource(R.drawable.balance_toe_taps);
+            exerciseListImage5.setImageResource(R.drawable.balance_tree_pose);
+        }
+        else if("Endurance".equals(exercise_type)){
+            exerciseListTitle1.setText("Crunch");
+            exerciseListTitle2.setText("Lunge");
+            exerciseListTitle3.setText("Plank");
+            exerciseListTitle4.setText("Push-up");
+            exerciseListTitle5.setText("Squat");
+
+            exerciseListImage1.setImageResource(R.drawable.endurance_crunch);
+            exerciseListImage2.setImageResource(R.drawable.endurance_lunge);
+            exerciseListImage3.setImageResource(R.drawable.endurance_plank);
+            exerciseListImage4.setImageResource(R.drawable.endurance_push_up);
+            exerciseListImage5.setImageResource(R.drawable.endurance_squat);
+
+        }
+        else if("Flexibility".equals(exercise_type)){
+            exerciseListTitle1.setText("Cat Cow");
+            exerciseListTitle2.setText("Groin Sretch");
+            exerciseListTitle3.setText("Hip Flexor Stretch");
+            exerciseListTitle4.setText("Side Lunge Stretch");
+            exerciseListTitle5.setText("Standing Biceps Stretch");
+
+            exerciseListImage1.setImageResource(R.drawable.flexibility_cat_cow);
+            exerciseListImage2.setImageResource(R.drawable.flexibility_groin_stretch);
+            exerciseListImage3.setImageResource(R.drawable.flexibility_hip_flexor_stretch);
+            exerciseListImage4.setImageResource(R.drawable.flexibility_side_lunge_stretch);
+            exerciseListImage5.setImageResource(R.drawable.flexibility_standing_bicep_stretch);
+        }
+        else if("Strength".equals(exercise_type)){
+            exerciseListTitle1.setText("Bicep Curl");
+            exerciseListTitle2.setText("Calf Raises");
+            exerciseListTitle3.setText("Dumbell Floor Press");
+            exerciseListTitle4.setText("Overhead Triceps Extension");
+            exerciseListTitle5.setText("Wall Push-up");
+
+            exerciseListImage1.setImageResource(R.drawable.strength_bicep_curl);
+            exerciseListImage2.setImageResource(R.drawable.strength_calf_raises);
+            exerciseListImage3.setImageResource(R.drawable.strength_dumbell_floor_press);
+            exerciseListImage4.setImageResource(R.drawable.strength_overhead_triceps_extension);
+            exerciseListImage5.setImageResource(R.drawable.strength_wall_push_up);
+
+        }
 
         btnBackExerciseList.setOnClickListener(new View.OnClickListener() {
             @Override
@@ -82,6 +141,176 @@ public class ExerciseList extends AppCompatActivity {
             }
         });
 
+        btnExerciseListNext1.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if ("Balance".equals(exercise_type)) {
+                    Intent intent = new Intent(ExerciseList.this, ViewExercise.class);
+
+                    intent.putExtra("exercise_title", "bird_dog");
+
+                    startActivity(intent);
+                }
+                else if("Endurance".equals(exercise_type)){
+                    Intent intent = new Intent(ExerciseList.this, ViewExercise.class);
+
+                    intent.putExtra("exercise_title", "crunch");
+
+                    startActivity(intent);
+                }
+                else if("Flexibility".equals(exercise_type)){
+                    Intent intent = new Intent(ExerciseList.this, ViewExercise.class);
+
+                    intent.putExtra("exercise_title", "cat_cow");
+
+                    startActivity(intent);
+                }
+                else if("Strength".equals(exercise_type)){
+                    Intent intent = new Intent(ExerciseList.this, ViewExercise.class);
+
+                    intent.putExtra("exercise_title", "bicep_curl");
+
+                    startActivity(intent);
+                }
+            }
+        });
+
+        btnExerciseListNext2.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if ("Balance".equals(exercise_type)) {
+                    Intent intent = new Intent(ExerciseList.this, ViewExercise.class);
+
+                    intent.putExtra("exercise_title", "lying_leg_raise");
+
+                    startActivity(intent);
+                }
+                else if("Endurance".equals(exercise_type)){
+                    Intent intent = new Intent(ExerciseList.this, ViewExercise.class);
+
+                    intent.putExtra("exercise_title", "lunge");
+
+                    startActivity(intent);
+                }
+                else if("Flexibility".equals(exercise_type)){
+                    Intent intent = new Intent(ExerciseList.this, ViewExercise.class);
+
+                    intent.putExtra("exercise_title", "groin_stretch");
+
+                    startActivity(intent);
+                }
+                else if("Strength".equals(exercise_type)){
+                    Intent intent = new Intent(ExerciseList.this, ViewExercise.class);
+
+                    intent.putExtra("exercise_title", "calf_raises");
+
+                    startActivity(intent);
+                }
+            }
+        });
+
+        btnExerciseListNext3.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if ("Balance".equals(exercise_type)) {
+                    Intent intent = new Intent(ExerciseList.this, ViewExercise.class);
+
+                    intent.putExtra("exercise_title", "single_leg_balance");
+
+                    startActivity(intent);
+                }
+                else if("Endurance".equals(exercise_type)){
+                    Intent intent = new Intent(ExerciseList.this, ViewExercise.class);
+
+                    intent.putExtra("exercise_title", "plank");
+
+                    startActivity(intent);
+                }
+                else if("Flexibility".equals(exercise_type)){
+                    Intent intent = new Intent(ExerciseList.this, ViewExercise.class);
+
+                    intent.putExtra("exercise_title", "hip_flexor_stretch");
+
+                    startActivity(intent);
+                }
+                else if("Strength".equals(exercise_type)){
+                    Intent intent = new Intent(ExerciseList.this, ViewExercise.class);
+
+                    intent.putExtra("exercise_title", "dumbell_floor_press");
+
+                    startActivity(intent);
+                }
+            }
+        });
+
+        btnExerciseListNext4.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if ("Balance".equals(exercise_type)) {
+                    Intent intent = new Intent(ExerciseList.this, ViewExercise.class);
+
+                    intent.putExtra("exercise_title", "toe_taps");
+
+                    startActivity(intent);
+                }
+                else if("Endurance".equals(exercise_type)){
+                    Intent intent = new Intent(ExerciseList.this, ViewExercise.class);
+
+                    intent.putExtra("exercise_title", "push_up");
+
+                    startActivity(intent);
+                }
+                else if("Flexibility".equals(exercise_type)){
+                    Intent intent = new Intent(ExerciseList.this, ViewExercise.class);
+
+                    intent.putExtra("exercise_title", "side_lunge_stretch");
+
+                    startActivity(intent);
+                }
+                else if("Strength".equals(exercise_type)){
+                    Intent intent = new Intent(ExerciseList.this, ViewExercise.class);
+
+                    intent.putExtra("exercise_title", "overhead_triceps_extension");
+
+                    startActivity(intent);
+                }
+            }
+        });
+
+        btnExerciseListNext5.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View v) {
+                if ("Balance".equals(exercise_type)) {
+                    Intent intent = new Intent(ExerciseList.this, ViewExercise.class);
+
+                    intent.putExtra("exercise_title", "tree_pose");
+
+                    startActivity(intent);
+                }
+                else if("Endurance".equals(exercise_type)){
+                    Intent intent = new Intent(ExerciseList.this, ViewExercise.class);
+
+                    intent.putExtra("exercise_title", "squat");
+
+                    startActivity(intent);
+                }
+                else if("Flexibility".equals(exercise_type)){
+                    Intent intent = new Intent(ExerciseList.this, ViewExercise.class);
+
+                    intent.putExtra("exercise_title", "standing_biceps_stretch");
+
+                    startActivity(intent);
+                }
+                else if("Strength".equals(exercise_type)){
+                    Intent intent = new Intent(ExerciseList.this, ViewExercise.class);
+
+                    intent.putExtra("exercise_title", "wall_push_up");
+
+                    startActivity(intent);
+                }
+            }
+        });
+
     }
 
     public void init(){
@@ -93,5 +322,23 @@ public class ExerciseList extends AppCompatActivity {
         nav_btn_calculator = findViewById(R.id.nav_btn_calculator_exercise_list);
         nav_btn_profile = findViewById(R.id.nav_btn_profile_exercise_list);
         nav_btn_exercises = findViewById(R.id.nav_btn_exercises_exercise_list);
+
+        exerciseListTitle1 = findViewById(R.id.exerciseListTitle1);
+        exerciseListTitle2 = findViewById(R.id.exerciseListTitle2);
+        exerciseListTitle3 = findViewById(R.id.exerciseListTitle3);
+        exerciseListTitle4 = findViewById(R.id.exerciseListTitle4);
+        exerciseListTitle5 = findViewById(R.id.exerciseListTitle5);
+
+        exerciseListImage1 = findViewById(R.id.exerciseListImage1);
+        exerciseListImage2 = findViewById(R.id.exerciseListImage2);
+        exerciseListImage3 = findViewById(R.id.exerciseListImage3);
+        exerciseListImage4 = findViewById(R.id.exerciseListImage4);
+        exerciseListImage5 = findViewById(R.id.exerciseListImage5);
+
+        btnExerciseListNext1 = findViewById(R.id.btnExerciseListNext1);
+        btnExerciseListNext2 = findViewById(R.id.btnExerciseListNext2);
+        btnExerciseListNext3 = findViewById(R.id.btnExerciseListNext3);
+        btnExerciseListNext4 = findViewById(R.id.btnExerciseListNext4);
+        btnExerciseListNext5 = findViewById(R.id.btnExerciseListNext5);
     }
 }
--- app/src/main/java/com/example/pasafit/ViewExercise.java
+++ app/src/main/java/com/example/pasafit/ViewExercise.java
@@ -7,13 +7,15 @@ import android.os.Bundle;
 import android.view.View;
 import android.widget.Button;
 import android.widget.ImageButton;
+import android.widget.ImageView;
+import android.widget.TextView;
 
 public class ViewExercise extends AppCompatActivity {
 
     private Button btnBackViewExercise;
-
     private ImageButton nav_btn_home, nav_btn_trackers, nav_btn_calculator, nav_btn_profile, nav_btn_exercises;
-
+    private ImageView imageViewExercise;
+    private TextView textViewHeadViewExercise, textViewDescriptionLowerExercise, textViewInstructionLowerExercise;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
@@ -21,11 +23,135 @@ public class ViewExercise extends AppCompatActivity {
 
         init();
 
+        Intent intent = getIntent();
+
+        String exercise_title = intent.getStringExtra("exercise_title");
+
+        if ("bird_dog".equals(exercise_title)) {
+            imageViewExercise.setImageResource(R.drawable.balance_bird_dog);
+            textViewHeadViewExercise.setText("Bird Dog");
+            textViewDescriptionLowerExercise.setText("The Bird Dog exercise is a core-strengthening move that enhances stability, balance, and coordination by engaging multiple muscle groups simultaneously.");
+            textViewInstructionLowerExercise.setText("Start on your hands and knees, with your wrists under your shoulders and knees under your hips. Extend your right arm forward and left leg back, keeping your body in a straight line. Hold for a few seconds, then return to the starting position. Repeat on the opposite side. Perform 10-15 repetitions on each side.");
+        }
+        else if ("crunch".equals(exercise_title)) {
+            imageViewExercise.setImageResource(R.drawable.endurance_crunch);
+            textViewHeadViewExercise.setText("Crunch");
+            textViewDescriptionLowerExercise.setText("Crunches target the abdominal muscles and help in building core strength.");
+            textViewInstructionLowerExercise.setText("Lie on your back with your knees bent and feet flat on the floor. Place your hands behind your head, elbows out. Lift your upper body towards your knees, engaging your abs. Lower back down without touching the ground. Perform 15-20 repetitions.");
+        }
+        else if ("cat_cow".equals(exercise_title)) {
+            imageViewExercise.setImageResource(R.drawable.flexibility_cat_cow);
+            textViewHeadViewExercise.setText("Cat-Cow");
+            textViewDescriptionLowerExercise.setText("The Cat-Cow stretch increases flexibility and relieves tension in the spine.");
+            textViewInstructionLowerExercise.setText("Start on your hands and knees. Inhale, arch your back, and lift your head and tailbone towards the ceiling (Cow). Exhale, round your back, and tuck your chin to your chest (Cat). Repeat for 10-15 cycles.");
+        }
+        else if ("bicep_curl".equals(exercise_title)) {
+            imageViewExercise.setImageResource(R.drawable.strength_bicep_curl);
+            textViewHeadViewExercise.setText("Bicep Curl");
+            textViewDescriptionLowerExercise.setText("Bicep curls strengthen the upper arms and improve muscle definition.");
+            textViewInstructionLowerExercise.setText("Stand with a dumbbell in each hand, arms at your sides. Keeping your elbows close to your body, curl the weights up to shoulder height. Lower back down slowly. Perform 12-15 repetitions.");
+        }
+        else if ("lying_leg_raise".equals(exercise_title)) {
+            imageViewExercise.setImageResource(R.drawable.balance_lying_leg_raise);
+            textViewHeadViewExercise.setText("Lying Leg Raise");
+            textViewDescriptionLowerExercise.setText("Lying leg raises target the lower abdominal muscles, improving core strength.");
+            textViewInstructionLowerExercise.setText("Lie on your back with your legs straight. Place your hands under your lower back for support. Lift your legs towards the ceiling, keeping them straight. Lower them back down slowly without touching the floor. Perform 10-15 repetitions.");
+        }
+        else if ("lunge".equals(exercise_title)) {
+            imageViewExercise.setImageResource(R.drawable.endurance_lunge);
+            textViewHeadViewExercise.setText("Lunge");
+            textViewDescriptionLowerExercise.setText("Lunges strengthen the legs and improve overall lower body endurance.");
+            textViewInstructionLowerExercise.setText("Stand with your feet together. Step forward with one leg, lowering your hips until both knees are bent at a 90-degree angle. Return to the starting position and repeat with the other leg. Perform 10-15 repetitions on each side.");
+        }
+        else if ("groin_stretch".equals(exercise_title)) {
+            imageViewExercise.setImageResource(R.drawable.flexibility_groin_stretch);
+            textViewHeadViewExercise.setText("Groin Stretch");
+            textViewDescriptionLowerExercise.setText("The groin stretch increases flexibility and reduces the risk of injury.");
+            textViewInstructionLowerExercise.setText("Sit with your feet together, knees bent and pointing out to the sides. Hold your feet with your hands and gently press your knees towards the floor. Hold the stretch for 20-30 seconds.");
+        }
+        else if ("calf_raises".equals(exercise_title)) {
+            imageViewExercise.setImageResource(R.drawable.strength_calf_raises);
+            textViewHeadViewExercise.setText("Calf Raises");
+            textViewDescriptionLowerExercise.setText("Calf raises strengthen the lower legs and improve stability.");
+            textViewInstructionLowerExercise.setText("Stand with your feet hip-width apart. Slowly raise your heels off the ground, standing on your toes. Lower back down with control. Perform 15-20 repetitions.");
+        }
+        else if ("single_leg_balance".equals(exercise_title)) {
+            imageViewExercise.setImageResource(R.drawable.balance_single_leg_balance);
+            textViewHeadViewExercise.setText("Single Leg Balance");
+            textViewDescriptionLowerExercise.setText("This exercise enhances balance and stability by engaging core and leg muscles.");
+            textViewInstructionLowerExercise.setText("Stand on one leg with the other leg bent at the knee. Hold this position for 20-30 seconds, then switch legs. Repeat 2-3 times on each side.");
+        }
+        else if ("plank".equals(exercise_title)) {
+            imageViewExercise.setImageResource(R.drawable.endurance_plank);
+            textViewHeadViewExercise.setText("Plank");
+            textViewDescriptionLowerExercise.setText("The plank is a core exercise that strengthens the entire body, focusing on the abs and back.");
+            textViewInstructionLowerExercise.setText("Start in a push-up position but with your weight on your forearms instead of your hands. Keep your body in a straight line from head to heels. Hold for 30-60 seconds.");
+        }
+        else if ("hip_flexor_stretch".equals(exercise_title)) {
+            imageViewExercise.setImageResource(R.drawable.flexibility_hip_flexor_stretch);
+            textViewHeadViewExercise.setText("Hip Flexor Stretch");
+            textViewDescriptionLowerExercise.setText("This stretch improves flexibility in the hip area and reduces lower back pain.");
+            textViewInstructionLowerExercise.setText("Kneel on one knee, with the other foot in front, creating a 90-degree angle. Push your hips forward gently. Hold the stretch for 20-30 seconds, then switch sides.");
+        }
+        else if ("dumbell_floor_press".equals(exercise_title)) {
+            imageViewExercise.setImageResource(R.drawable.strength_dumbell_floor_press);
+            textViewHeadViewExercise.setText("Dumbbell Floor Press");
+            textViewDescriptionLowerExercise.setText("This exercise targets the chest and triceps, improving upper body strength.");
+            textViewInstructionLowerExercise.setText("Lie on your back with a dumbbell in each hand, elbows on the floor. Press the weights up until your arms are fully extended. Lower them back down with control. Perform 10-12 repetitions.");
+        }
+        else if ("toe_taps".equals(exercise_title)) {
+            imageViewExercise.setImageResource(R.drawable.balance_toe_taps);
+            textViewHeadViewExercise.setText("Toe Taps");
+            textViewDescriptionLowerExercise.setText("Toe taps are a low-impact exercise that enhances coordination and leg strength.");
+            textViewInstructionLowerExercise.setText("Stand in front of a step or low platform. Tap one foot on the platform, then switch feet quickly. Continue alternating for 30-60 seconds.");
+        }
+        else if ("push_up".equals(exercise_title)) {
+            imageViewExercise.setImageResource(R.drawable.endurance_push_up);
+            textViewHeadViewExercise.setText("Push Up");
+            textViewDescriptionLowerExercise.setText("Push-ups are a fundamental exercise that builds strength in the chest, shoulders, and triceps.");
+            textViewInstructionLowerExercise.setText("Start in a plank position with your hands under your shoulders. Lower your body until your chest nearly touches the floor. Push back up to the starting position. Perform 10-15 repetitions.");
+        }
+        else if ("side_lunge_stretch".equals(exercise_title)) {
+            imageViewExercise.setImageResource(R.drawable.flexibility_side_lunge_stretch);
+            textViewHeadViewExercise.setText("Side Lunge Stretch");
+            textViewDescriptionLowerExercise.setText("This stretch increases flexibility and mobility in the hips and legs.");
+            textViewInstructionLowerExercise.setText("Stand with your feet wide apart. Shift your weight to one side, bending your knee and keeping the other leg straight. Hold the stretch for 20-30 seconds, then switch sides.");
+        }
+        else if ("overhead_triceps_extension".equals(exercise_title)) {
+            imageViewExercise.setImageResource(R.drawable.strength_overhead_triceps_extension);
+            textViewHeadViewExercise.setText("Overhead Triceps Extension");
+            textViewDescriptionLowerExercise.setText("This exercise targets the triceps, enhancing upper arm strength.");
+            textViewInstructionLowerExercise.setText("Stand with your feet shoulder-width apart. Hold a dumbbell with both hands and lift it overhead. Lower the weight behind your head by bending your elbows. Extend your arms back up. Perform 12-15 repetitions.");
+        }
+        else if ("tree_pose".equals(exercise_title)) {
+            imageViewExercise.setImageResource(R.drawable.balance_tree_pose);
+            textViewHeadViewExercise.setText("Tree Pose");
+            textViewDescriptionLowerExercise.setText("Tree Pose improves balance and stability while strengthening the legs.");
+            textViewInstructionLowerExercise.setText("Stand on one leg, with the other foot placed on the inner thigh or calf (avoid the knee). Bring your hands together in front of your chest. Hold for 20-30 seconds, then switch legs.");
+        }
+        else if ("squat".equals(exercise_title)) {
+            imageViewExercise.setImageResource(R.drawable.endurance_squat);
+            textViewHeadViewExercise.setText("Squat");
+            textViewDescriptionLowerExercise.setText("Squats are a full-body exercise that primarily targets the legs and glutes.");
+            textViewInstructionLowerExercise.setText("Stand with your feet shoulder-width apart. Lower your body by bending your knees and hips, keeping your back straight. Return to the starting position. Perform 15-20 repetitions.");
+        }
+        else if ("standing_biceps_stretch".equals(exercise_title)) {
+            imageViewExercise.setImageResource(R.drawable.flexibility_standing_bicep_stretch);
+            textViewHeadViewExercise.setText("Standing Biceps Stretch");
+            textViewDescriptionLowerExercise.setText("This stretch targets the biceps and shoulders, improving flexibility.");
+            textViewInstructionLowerExercise.setText("Stand with your feet shoulder-width apart. Extend one arm out to the side, thumb pointing down. Gently press your hand down with the opposite hand. Hold for 20-30 seconds, then switch sides.");
+        }
+        else if ("wall_push_up".equals(exercise_title)) {
+            imageViewExercise.setImageResource(R.drawable.strength_wall_push_up);
+            textViewHeadViewExercise.setText("Wall Push Up");
+            textViewDescriptionLowerExercise.setText("Wall push-ups are a beginner-friendly exercise that strengthens the chest and arms.");
+            textViewInstructionLowerExercise.setText("Stand facing a wall, arms extended and hands on the wall. Bend your elbows and lean towards the wall, then push back to the starting position. Perform 15-20 repetitions.");
+        }
+
         btnBackViewExercise.setOnClickListener(new View.OnClickListener() {
             @Override
             public void onClick(View v) {
-                Intent intent = new Intent(ViewExercise.this, ExerciseList.class);
-                startActivity(intent);
+                onBackPressed();
             }
         });
 
@@ -84,6 +210,13 @@ public class ViewExercise extends AppCompatActivity {
         nav_btn_calculator = findViewById(R.id.nav_btn_calculator_view_exercise);
         nav_btn_profile = findViewById(R.id.nav_btn_profile_view_exercise);
         nav_btn_exercises = findViewById(R.id.nav_btn_exercise_view_exercise);
+
+        imageViewExercise = findViewById(R.id.imageViewExercise);
+
+        textViewDescriptionLowerExercise = findViewById(R.id.textViewDescriptionLowerExercise);
+        textViewInstructionLowerExercise = findViewById(R.id.textViewInstructionLowerExercise);
+
+        textViewHeadViewExercise = findViewById(R.id.textViewHeadViewExercise);
 
     }
 }
